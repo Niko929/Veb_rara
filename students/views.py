fb86f53e14b6475443a6a323351fdcab1ccb7912
MSI <ekaterinanikolskaa2@gmail.com>
2025-07-11 13:13:08 +0400

    HTLM open ORM

---
+++ students/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'students/home.html')
+
+# def contac(request):
+#     if request.method == 'GET':
+#         return render(request, 'app/data.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+        # Обработка данных формы
+        return HttpResponse(f"Данные отправлены!{name}")
+    return render(request, 'students/contact.html')
+
